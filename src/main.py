1204a37bd20c3c7906920b83d3734cc15d054710
Aditya Kumar <adityak007mr@gmail.com>
2024-10-26 09:22:30 +0530

    added new treks, code cleanup

--- src/main.py
+++ src/main.py
@@ -9,8 +9,6 @@ load_dotenv()
 
 parser=argparse.ArgumentParser()
 
-# parser.add_argument("--org", type=str, required=True, help="Organisation for which the generator would run")
-# parser.add_argument("--input", type=str, required=True, help="Input for the generator")
 parser.add_argument("--output", type=str, required=True, help="Output file for the generated data")
 
 # Ensure Scrapy project is in the Python path
@@ -46,75 +44,6 @@ def clean_data(data):
         })
     outputTreks["org"] = KEY
     outputTreks["treks"] = generatedTreks
-
-
-    # if org == "IH":
-    #     print(f"Found {len(treksList)} treks")
-    #     URL = os.getenv('IH_URL')
-    #     IH_KEY = os.getenv('IH_KEY')
-    #     generatedTreks = []
-    #     for trekInfo in treksList:
-    #         generatedTreks.append({
-    #             "uuid": str(uuid.uuid4()),
-    #             "title": trekInfo["title"],
-    #             "uid": trekInfo["uid"],
-    #             "url": URL + "/" + trekInfo["uid"],
-    #             "elevation": trekInfo["elevation"],
-    #             "duration": trekInfo["duration"],
-    #             "cost": trekInfo["cost"],
-    #             "difficulty": trekInfo["difficulty"],
-    #             "location": trekInfo["location"],
-    #             "distance": trekInfo["distance"],
-    #             "bestTimeToTarget": "",
-    #             "tags": []
-    #         })
-    #     outputTreks["org"] = IH_KEY
-    #     outputTreks["treks"] = generatedTreks
-    # elif org == "TTH":
-    #     print(f"Found {len(treksList)} treks")
-    #     URL = os.getenv('TTH_URL')
-    #     TTH_KEY = os.getenv('TTH_KEY')
-    #     generatedTreks = []
-    #     for trekInfo in treksList:
-    #         generatedTreks.append({
-    #             "uuid": str(uuid.uuid4()),
-    #             "title": trekInfo["title"],
-    #             "uid": trekInfo["uid"],
-    #             "url": URL + "/" + trekInfo["uid"],
-    #             "elevation": trekInfo["elevation"],
-    #             "duration": trekInfo["duration"],
-    #             "cost": trekInfo["cost"],
-    #             "difficulty": trekInfo["difficulty"],
-    #             "location": trekInfo["location"],
-    #             "distance": trekInfo["distance"],
-    #             "bestTimeToTarget": "",
-    #             "tags": []
-    #         })
-    #     outputTreks["org"] = TTH_KEY
-    #     outputTreks["treks"] = generatedTreks
-    # elif org == "BKT":
-    #     print(f"Found {len(treksList)} treks")
-    #     URL = os.getenv('BKT_URL')
-    #     BKT_KEY = os.getenv('BKT_KEY')
-    #     generatedTreks = []
-    #     for trekInfo in treksList:
-    #         generatedTreks.append({
-    #             "uuid": str(uuid.uuid4()),
-    #             "title": trekInfo["title"],
-    #             "uid": trekInfo["uid"],
-    #             "url": URL + "/" + trekInfo["uid"],
-    #             "elevation": trekInfo["elevation"],
-    #             "duration": trekInfo["duration"],
-    #             "cost": trekInfo["cost"],
-    #             "difficulty": trekInfo["difficulty"],
-    #             "location": trekInfo["location"],
-    #             "distance": trekInfo["distance"],
-    #             "bestTimeToTarget": "",
-    #             "tags": []
-    #         })
-    #     outputTreks["org"] = BKT_KEY
-    #     outputTreks["treks"] = generatedTreks
-
     return outputTreks
 
 def save_json(data, output_file):
@@ -130,30 +59,18 @@ def get_all_data_files(base_path):
 
 def main(output_path):
     print("Skipping File Download")
-    # print(f"Performing the operation for {org}")
-
-    # orgs_list = org.split(",")
     input_path_list = get_all_data_files("./data")
-    # print(input_path_list)
-    # if len(orgs_list) is not len(input_path_list):
-    #     print("Please enter correct input")
-    #     print(f"Orgs: {orgs_list} should be equivalent to input paths provided: {input_path_list}")
-    #     exit(1)
-    
+
     complied_output = []
     for i in range(len(input_path_list)):
         parsed_data = parse_json(input_path_list[i])
-        # print(parsed_data)
         cleaned_data = clean_data(parsed_data)
         complied_output.append(cleaned_data)
 
     save_json(complied_output, output_path)
 
-    # print(complied_output)
-
     db_handler = DBHandler(complied_output)
     db_handler.update_data()
-    print(result)
 
 if __name__ == "__main__":
     args=parser.parse_args()
